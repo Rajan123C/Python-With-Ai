a3d2d17f2f5ae8066992a09b362e59bb10a1d9e3
Rajan123C <rc7110973@gmail.com>
2024-07-26 22:10:52 +0530

    Merged unrelated histories

---
+++ Operators.py
@@ -0,0 +1,66 @@
+# Arithmetic Operator (+,-,*,/,%,**)
+a = 6
+b = 2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b) #remender
+print(a ** b) #a^b we are doing hear
+
+# Relational / Comperison Operator (==,!=,>,<,>=,<=)
+
+a = 30
+b = 20
+
+print(a == b) #False
+print(a != b) #True
+print(a <= b) #False
+print(a < b) #False
+print(a >= b) #True
+print(a > b) #True
+
+
+
+# Assignment Operator (=,+=,-=,*=,/=,%=.**=)
+
+num1 = 10
+num1 += 10
+print("Num :",num1)
+
+num2 = 20
+num2 -= 10
+print("Num :",num2)
+
+num3 = 30
+num3 *= 10
+print("Num :",num3)
+
+num4 = 30
+num4 /= 10
+print("Num :",num4)
+
+num5 = 30
+num5 %= 10
+print("Num :",num5)
+
+num6 = 30
+num6 **= 10
+print("Num :",num6)
+
+# Logical Operator (not,and,or)
+
+a = 50
+b = 30
+
+print(not False)
+print(not True)
+
+val1 = True
+val2 = True
+
+print("And Operator :",val1 and val2)
+print("OR Operator :", val1 or val2)
+
+print("OR Operator :", (a == b) or (a < b))
